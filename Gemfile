2c21ea2e8723be7715d21cdbc56a870109c2e167
Matt McCoy <mdmccoy@gmail.com>
2018-03-01 19:15:57 -0500

    User login (#9)
    * facebook oauth working
    
    * rspec installed. working ok
    
    * adding tests. Feature test not passing for user auth.
    
    * user login test green

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'rspec-rails'
+  gem "factory_bot_rails", "~> 4.0"
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'pages#home'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
