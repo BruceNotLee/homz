9fba44a838fdcdfd8d9890852d570a8aab2606e2
Joe Pollis <34250239+jpollis@users.noreply.github.com>
2018-03-02 16:44:42 -0500

    Login (#13)
    * zip code is being pulled from browser
    
    * commented out bruce code. Login working, geolocation working
    
    * login logout working. options to login or use current location after load page..
    
    * use current location redirects to feed
    
    * redirect from facebook auth to feed page fixed

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || root_path
+    # request.env['omniauth.origin'] || root_path
+    feed_path
   end
 end
