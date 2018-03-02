9fb1696ae0ffe9a53c0831d5b6da41a0e7fce1f0
Joe <jpollis.slp@gmail.com>
2018-03-01 21:49:36 -0500

    commented out bruce code. Login working, geolocation working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,17 +19,17 @@
 //= require_tree .
 
 $( document ).on('turbolinks:load', function() {
-  $('.dropdown-button').dropdown({
-     inDuration: 300,
-     outDuration: 225,
-     constrainWidth: false, // Does not change width of dropdown to that of the activator
-     hover: true, // Activate on hover
-     gutter: 0, // Spacing from edge
-     belowOrigin: false, // Displays dropdown below the button
-     alignment: 'left', // Displays dropdown with edge aligned to the left of button
-     stopPropagation: false // Stops event propagation
-   }
- );
+ //  $('.dropdown-button').dropdown({
+ //     inDuration: 300,
+ //     outDuration: 225,
+ //     constrainWidth: false, // Does not change width of dropdown to that of the activator
+ //     hover: true, // Activate on hover
+ //     gutter: 0, // Spacing from edge
+ //     belowOrigin: false, // Displays dropdown below the button
+ //     alignment: 'left', // Displays dropdown with edge aligned to the left of button
+ //     stopPropagation: false // Stops event propagation
+ //   }
+ // );
 
  console.log("It works on each visit!")
  var lat;
