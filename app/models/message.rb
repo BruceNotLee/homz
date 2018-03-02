4930c3d85f6119c595d22ff3a4ee9b680bf7a563
Matt McCoy <mdmccoy@gmail.com>
2018-03-01 20:48:03 -0500

    Setup Action Cable based Chat (#10)
    Basic chat rooms and messaging working.
    
    Fix error in CSS

---
+++ app/models/message.rb
@@ -0,0 +1,12 @@
+class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :chat_room
+
+  validates :body, presence: true, length: {minimum: 2, maximum: 1000}
+
+  def timestamp
+    created_at.strftime('%H:%M:%S %d %B %Y')
+  end
+
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
+end
