f6318e7c3af5e3ab512bc5f2c86e2a7fc208fc33
BruceNotLee <bruce.lesko@outlook.com>
2018-03-09 18:59:49 -0500

    * Larger paginator gif

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -4,7 +4,7 @@ jQuery(function() {
       var more_pictures_url;
       more_pictures_url = $('.next_page').attr('href');
       if (more_pictures_url && $(window).scrollTop() > $(document).height() - $(window).height() - 80) {
-        $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />');
+        $('.pagination').html('<img src="/assets/gif-loader-image.gif" alt="Loading..." title="Loading..." />');
         $.getScript(more_pictures_url);
       }
       return;
