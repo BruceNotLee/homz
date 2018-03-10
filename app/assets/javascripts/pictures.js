6f1c0a6044499d099cecd48141f53924aefbe9a4
Joe <jpollis.slp@gmail.com>
2018-03-10 12:34:24 -0500

    modal closing on yes and no clicks. calendar working.

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -7,19 +7,32 @@ $(document).on('turbolinks:load', function(){
   // $('#modal1').modal('open')
   // $('.datepicker').attr("placeholder", "schedule here");
   $('.datepicker').pickadate({
-  selectMonths: true, // Creates a dropdown to control month
-  selectYears: 15, // Creates a dropdown of 15 years to control year,
-  today: 'Today',
-  clear: 'Clear',
-  close: 'Ok',
-  closeOnSelect: true // Close upon selecting a date,
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: true // Close upon selecting a date,
   });
 
   $('.datepicker').on("change", function(){
     var select = $(this).val();
     // alert(select);
-    $(".appointment").append(select);
+    var apptBody = "An email will be sent to the seller to schedule an appointment for "
+    $(".appointment").text(apptBody + select);
     $('.modal').modal();
     $('#modal2').modal('open');
   });
+
+  $('#yes').click(function(){
+    // $('.modal').modal();
+    $('#modal2').modal('close');
+  });
+
+  $('#no').click(function(){
+    $('#modal2').modal('close');
+  });
+
+
+
 });
