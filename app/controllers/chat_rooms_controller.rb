deb5e817c31b045eddf0571d488af07be2cfb37b
Joe <jpollis.slp@gmail.com>
2018-03-11 16:38:10 -0400

    holy smokes. the datepicker auto messaging/scheduling is working.

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -9,21 +9,49 @@ class ChatRoomsController < ApplicationController
     @chat_room = ChatRoom.new
   end
 
-  def create
-    @chat_room =  ChatRoom.find_or_create_by(title: params[:title]) do |c|
-      c.title = params[:title]
-      c.user_id = current_user.id
-    end
 
-    if @chat_room.save
-      # ChatInvite.find(chat_room_id: @chat_room.id, user_id: params[:pic_owner_id])
-      ChatInvite.find_or_create_by(chat_room_id: @chat_room.id, user_id: params[:pic_owner_id]) do |ci|
-        ci.chat_room_id = @chat_room.id
-        ci.user_id = params[:pic_owner_id]
+  def create
+    respond_to do |format|
+      format.html do
+        @chat_room =  ChatRoom.find_or_create_by(title: params[:title]) do |c|
+          c.title = params[:title]
+          c.user_id = current_user.id
+        end
+
+        if @chat_room.save
+
+          # ChatInvite.find(chat_room_id: @chat_room.id, user_id: params[:pic_owner_id])
+          ChatInvite.find_or_create_by(chat_room_id: @chat_room.id, user_id: params[:pic_owner_id]) do |ci|
+            ci.chat_room_id = @chat_room.id
+            ci.user_id = params[:pic_owner_id]
+          end
+          redirect_to @chat_room
+        else
+          render 'new'
+        end
       end
-      redirect_to @chat_room
-    else
-      render 'new'
+      format.js do
+        @chat_room =  ChatRoom.find_or_create_by(title: params[:title]) do |c|
+          c.title = params[:title]
+          c.user_id = current_user.id
+        end
+
+        if @chat_room.save
+
+          # ChatInvite.find(chat_room_id: @chat_room.id, user_id: params[:pic_owner_id])
+          ChatInvite.find_or_create_by(chat_room_id: @chat_room.id, user_id: params[:pic_owner_id]) do |ci|
+            ci.chat_room_id = @chat_room.id
+            ci.user_id = params[:pic_owner_id]
+          end
+          # redirect_to @chat_room
+          # redirect_to "/chat_rooms/#{@chat_room.id}"
+          Message.create(chat_room_id:@chat_room.id, user:current_user, body: "Hello, I would like to schedule a visit to see your property on #{params[:date]}. Does that work for you?")
+
+          render :js => "window.location.href = '/chat_rooms/#{@chat_room.id}'"
+        else
+          render 'new'
+        end
+      end
     end
   end
 
